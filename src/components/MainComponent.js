08dd48b9d998c2807b58941b64803b738af50a3d
Oyanna <moonlightreprise@gmail.com>
2020-08-19 17:55:07 +0600

    Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponet';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import HeaderComponet from './HeaderComponent';
 import Footer from './FooterComponent'
@@ -10,6 +11,7 @@ import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
 import {LEADERS} from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 
 class Main extends Component {
@@ -29,20 +31,26 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return(
-          <Home 
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
+        <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
+    
 
-    const DishWithId = (match) => {
+    const DishWithId = ({match}) => {
       return(
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
-    );
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
     };
+
+    
+
+    
+    
     return (
       <div>
           <HeaderComponet />
@@ -51,6 +59,7 @@ class Main extends Component {
                <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
                <Route path='/menu/:dishId' component={DishWithId} />
                <Route exact path='/contactus' component={Contact} />
+               <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
                <Redirect to="/home" />
              </Switch>
           <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-    function RenderMenuItem ({dish}) {
+    function RenderMenuItem ({dish, onClick}) {
         return (
             <Card>
-                <Link to={'/menu/${dish.id}'} >
+                <Link to={`/menu/${dish.id}`} >
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
